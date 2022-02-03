733596b5f1c8ed5971dfb0fb5d6ffc5b6b236124
AmadeusNoah <79079061+AmadeusNoah@users.noreply.github.com>
2022-02-03 15:31:32 +0100

    build done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF5851"
+      },
+      
+      fontFamily: {
+        Montserrat: "'Montserrat', sans-serif",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
